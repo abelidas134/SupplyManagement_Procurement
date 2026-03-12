75c38ca21519845f4a4d2ffdae0173c8e5fdc565
Mickey <abelidasmickey134@gmail.com>
2026-03-12 08:04:49 +0800

    Sample Back Commit only

--- Program.cs
+++ Program.cs
@@ -78,7 +78,7 @@ namespace SupplyManagement_Procurement
             Console.WriteLine("\n============================");
             Console.WriteLine("Products Category:");
             Console.WriteLine("============================");
-            Console.WriteLine("A. CANS" + "\nB. HYGIENE PRODUCTS" + "\nC. "
+            Console.WriteLine("A. CANS" + "\nB. HYGIENE PRODUCTS" + "\nC. NOODLES"
             + "\nD. BISCUITS/CHICHIRYA" + "\nE. SOFTDRINKS");
         }
 
